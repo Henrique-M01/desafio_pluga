56a96b6e95b6987fbf5f93aef3f5cc70546d31d1
Henrique Martins <87535781+Henrique-M01@users.noreply.github.com>
2023-11-14 20:27:13 -0300

    Updates

---
+++ precache-manifest.4d1653ca0fa0e3266fd87de41926cec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a27063f25147deb8f921f2ba22a7106",
+    "url": "/Henrique-M01/desafio_pluga/index.html"
+  },
+  {
+    "revision": "d29ba9a22bebf8ffe00e",
+    "url": "/Henrique-M01/desafio_pluga/static/css/main.84e1868c.chunk.css"
+  },
+  {
+    "revision": "a3ec152b2342df88bd1e",
+    "url": "/Henrique-M01/desafio_pluga/static/js/2.e414fc16.chunk.js"
+  },
+  {
+    "revision": "d29ba9a22bebf8ffe00e",
+    "url": "/Henrique-M01/desafio_pluga/static/js/main.81d7b442.chunk.js"
+  },
+  {
+    "revision": "acfafaf84844c585e2d8",
+    "url": "/Henrique-M01/desafio_pluga/static/js/runtime~main.e433505b.js"
+  }
+]);
